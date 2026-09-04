bf683984cf82665caad221617bdab0936aedf3f6
Cory Simon <Cory.Simon@oregonstate.edu>
2026-09-03 17:38:44 -0700

    polish

--- chi_test.jl
+++ chi_test.jl
@@ -7,20 +7,25 @@ using InteractiveUtils
 # ╔═╡ 85db4222-a7d1-11f1-8861-9b8b1c1400b0
 begin
 	import Pkg; Pkg.activate()
-	using UnicodePlots, StatsBase, CairoMakie
-	update_theme!(theme_minimal())
+	using UnicodePlots, StatsBase, CairoMakie, Random
+	Random.seed!(1337)
+	set_theme!(theme_minimal())
 	update_theme!(fontsize=20, linewidth=3)
 end
 
 # ╔═╡ e8b6036c-91f1-4c75-91d1-c1a5f6cbf9b6
 md"
 
-!!! note
-	topic: simulation-based approach for hypothesis testing to decide whether an observed distribution over categories differs from an expected distribution. I learned this from the excellent textbook \"Understanding Data\" by Alan Garfinkel and Yina Guo.
+# hypothesis testing for a distribution over categories
+
+💡 topic: a simulation-based approach for testing a hypothesis that an observed distribution over categories differs from an expected distribution. 
+
+!!! note \"Reference\"
+	I learned this from the excellent textbook \"Understanding Data\" by Alan Garfinkel and Yina Guo.
 
 
 ## customers pick drinks at a bar
-just opened a bar! we serve three drinks. each individual customer lines up and orders a single drink.
+say we just opened a bar, and we serve three drinks. each customer orders a single drink.
 "
 
 # ╔═╡ b42b91fc-e910-47b6-bd83-7fcbd679633d
@@ -29,21 +34,20 @@ drinks = ["🍷", "🍺", "🍸"]
 # ╔═╡ 80df9ccd-1e9b-4088-80a0-dcf36672cff9
 md"
 ## null hypothesis
-we have a null hypothesis: all drinks are equally preferable. under this hypothesis, a customer selects a drink according to the uniform distribution over drinks.
+our null hypothesis is: all drinks are equally preferable; i.e. a customer picks a drink at random.
 "
 
 # ╔═╡ af287958-b529-48ce-bc68-3ba4e3fd36fe
-p_null = Dict("🍷" => 1/3, "🍺" => 1/3, "🍸" => 1/3) # drink-selection probabilities
+# null hypothesis: uniform distribution over the drinks
+p_null = Dict("🍷" => 1/3, "🍺" => 1/3, "🍸" => 1/3) 
 
 # ╔═╡ 29296657-243f-488a-b942-55f5b7587903
 md"
-under the null hypothesis, we can simulate outcomes of customers purchasing drinks. 
-
-we assume each customer chooses a drink independently of the others. (not true if, e.g., customers behind me see me choose 🍷 then think \"the cool ppl are choosing🍷, I'll order that too.\")
+under the null hypothesis, we can simulate outcomes of customers purchasing drinks, if we assume each customer chooses a drink independently of the others.
 "
 
 # ╔═╡ 0e2bc51b-35bc-44d8-8934-52a8fa952d7f
-function simulate_null(n_customers)
+function simulate_null(n_customers::Int, p_null::Dict{String, Float64})
 	data = Dict(drink => 0 for drink in drinks)
 
 	ps = ProbabilityWeights([p_null[drink] for drink in drinks])
@@ -59,7 +63,7 @@ end
 md"simulation \#1"
 
 # ╔═╡ d805d8af-baf8-43f3-8547-13f4e8de51fa
-sim_data = simulate_null(30)
+sim_data = simulate_null(30, p_null)
 
 # ╔═╡ 516584c9-2071-4a15-a218-6792ca6c8f27
 UnicodePlots.barplot(drinks, [sim_data[drink] for drink in drinks])
@@ -68,18 +72,18 @@ UnicodePlots.barplot(drinks, [sim_data[drink] for drink in drinks])
 md"simulation \#2"
 
 # ╔═╡ a042d091-7aa4-47d0-b1c4-8548347a84e7
-sim_data2 = simulate_null(30)
+sim_data2 = simulate_null(30, p_null)
 
 # ╔═╡ ccc5e09d-da8a-47df-a3cd-521676597ab2
 UnicodePlots.barplot(drinks, [sim_data2[drink] for drink in drinks])
 
 # ╔═╡ b7e8e6da-a6dd-4a86-b0f0-82628f4063b1
-md"the outcome of the simulation is stochastic, and usually the drink counts are not the same." 
+md"the outcome of the simulation is stochastic, and usually the drink counts are not the same despite the assumed uniform distribution." 
 
 # ╔═╡ 638a7b16-d482-4c80-8e35-efce14408ba0
 md"
 ## data
-on our first day, 30 customers visited our bar. each purchased a drink. here is the empirical distribution."
+on our first day, 30 customers visited our bar. each purchased a drink. here is the empirical distribution over drinks."
 
 # ╔═╡ 6f6f0451-751c-433a-8c5a-5d2048a671cb
 data = Dict("🍷" => 14, "🍺" => 7, "🍸" => 9)
@@ -93,7 +97,7 @@ UnicodePlots.barplot(drinks, [data[drink] for drink in drinks])
 # ╔═╡ 196a379d-cae2-4aca-a1fb-84a2690aeb84
 md"## the statistical question
 
-does the observed distribution among the drinks differ in a statistically signficant way from the expected distribution under the null hypothesis? sure, our data does not have equal counts of drinks, but neither do the simualted data sets under the null hypothesis!
+does the observed distribution among the drinks differ in a statistically significant way from the expected distribution under the null hypothesis? sure, our data does not have equal counts of drinks, but neither do the simulated data sets under the null hypothesis!
 "
 
 # ╔═╡ e14e0adc-872e-4304-bb14-452fea9759c4
@@ -107,7 +111,7 @@ to measure the difference between an observed distribution and the expected dist
 "
 
 # ╔═╡ 7c5d8b53-a0aa-41d0-9f97-69cd45af8305
-χ_obs = sum(abs(data[drink] - n_customers / length(drinks)) for drink in drinks)
+χ_obs = sum(abs(data[drink] - Int(n_customers / length(drinks))) for drink in drinks)
 
 # ╔═╡ 6cb646c7-ff52-420d-9da5-32624caef9b3
 md"## distribution of test statistic under null hypothesis
@@ -122,9 +126,9 @@ n_sims = 10_000
 # ╔═╡ 100a41a0-4b68-4c47-8be8-661a637867f3
 χs_null = [
 	begin
-		local sim_data = simulate_null(n_customers)
+		local sim_data = simulate_null(n_customers, p_null)
 		χ = sum(
-			abs(sim_data[drink] - n_customers / length(drinks)) 
+			abs(sim_data[drink] - Int(n_customers / length(drinks)))
 			for drink in drinks
 		)
 	end
@@ -133,38 +137,48 @@ n_sims = 10_000
 
 # ╔═╡ 1b93b0ce-4ed5-463d-9743-b443a2426bc0
 begin
-	fig = Figure()
+	χ_counts = countmap(χs_null)
+	χ_vals = sort(collect(keys(χ_counts)))
+	χ_freq = [χ_counts[χ] for χ in χ_vals]
+
+	fig = Figure(size=(500, 400))
 	ax = Axis(
-		fig[1, 1], 
-		xlabel="χ", ylabel="# outcomes", 
-		limits=(0, nothing, 0, nothing)
+		fig[1, 1],
+		xlabel=L"\chi",
+		ylabel="# simulated outcomes\nunder the null hypothesis",
+		xticks=0:4:maximum(χ_vals),
+		limits=(-1.5, nothing, 0, nothing)
 	)
-	hist!(χs_null)
-	vlines!([χ_obs], color=Cycled(2), label=rich("χ", subscript("obs")))
-	axislegend()
+	CairoMakie.barplot!(ax, χ_vals, χ_freq, width=1, color="green")
+	vlines!(ax, [χ_obs], color=Cycled(2), label=L"\chi_{obs}")
+	axislegend(ax)
 	fig
 end
 
 # ╔═╡ c97eab42-ce38-425c-824e-0c476d273d11
-md"the observed test statistic falls pretty much in the middle of the distribution of the test statistic under the null hypothesis. that means our observed test statistic is consistent with the null hypothesis, actually. so we lack strong evidence to reject the null hypothesis based on our drink picks data with these 30 customers.
+md"the observed test statistic falls inside the bulk of the distribution of the test statistic under the null hypothesis. that means our observed test statistic is consistent with the null hypothesis. so we lack strong evidence to reject the null hypothesis based on our drink-selection data pertaining to these 30 customers.
 
 ## p-value
 
-the p-value is the probability that we'd get a test statistic equal to or more extreme than our observed test statistic if the null hypothesis were true. since the p-value is quite big, we cannot reject the null hypothesis.
-
-if we collected way more data (i.e. more customers), we might have the power to reject the null hypothesis. not with this small data set.
+the p-value is the probability that we'd get a test statistic equal to or more extreme than our observed test statistic if the null hypothesis were true.
 "
 
 # ╔═╡ be972bad-8c88-4aa5-a5a2-50a829aa18c0
 p_val = sum(χs_null .≥ χ_obs) / n_sims
 
+# ╔═╡ a6661bc2-b568-412f-90a0-a608293ab71b
+md"since the p-value is quite big, we do not reject the null hypothesis.
+
+this doesn't mean the null hypothesis is true. if there really were underlying preference differences among the drinks, a more sizable data set (i.e. more customers) would give us the power to reject the null hypothesis. this is quite a small data set.
+"
+
 # ╔═╡ 773db6a7-6f75-4a29-a49b-b7426e5b8d91
 md"## caveats
 
-* we assumed independent decisions among the customers. probably the drinks that earlier customers chose influence the choices of later customers. 
-* a single customer might order several drinks over the night. maybe the customer prefers variety or sticks to the same drink.
-* drink preferences might change based on time of day, weather, season, day of the week.
-* introducing a new drink might shift preferences. check out Luce's choice axiom.
+* we assumed independent decisions among the customers. probably the drinks chosen by earlier customers influence the choices of later customers. for example, customers may see me drinking 🍷 then think \"the cool ppl are choosing🍷, I'll order that too.\" people might order in groups too---and influence each others' decisions.
+* a single customer might order several drinks over the night. maybe the customer prefers variety or sticks to the same drink. the Chumbawamba song _Tubthumping_ suggests variety.
+* drink preferences might change based on time of day, weather, season, and day of the week.
+* introducing a new drink might shift preferences. look up Luce's choice axiom, regularity, and the decoy effect.
 "
 
 # ╔═╡ Cell order:
@@ -195,4 +209,5 @@ md"## caveats
 # ╠═1b93b0ce-4ed5-463d-9743-b443a2426bc0
 # ╟─c97eab42-ce38-425c-824e-0c476d273d11
 # ╠═be972bad-8c88-4aa5-a5a2-50a829aa18c0
+# ╟─a6661bc2-b568-412f-90a0-a608293ab71b
 # ╟─773db6a7-6f75-4a29-a49b-b7426e5b8d91
